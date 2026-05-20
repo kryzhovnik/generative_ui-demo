62fdac01a3f571117feabe24597549e4e43c6a32
Andrey Samsonov <andrey.samsonov@gmail.com>
2026-05-20 22:40:34 +0200

    Fix rubocop space-in-array-literals in WeatherCode

--- app/lib/weather/weather_code.rb
+++ app/lib/weather/weather_code.rb
@@ -1,8 +1,8 @@
 module Weather
   module WeatherCode
     ICONS = {
-      "sunny"  => [0, 1],
-      "cloudy" => [2, 3, 45, 48],
+      "sunny"  => [ 0, 1 ],
+      "cloudy" => [ 2, 3, 45, 48 ],
       "rainy"  => (51..67).to_a + (80..82).to_a,
       "snowy"  => (71..77).to_a + (85..86).to_a,
       "stormy" => (95..99).to_a
